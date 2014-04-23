c0e16b08ee4fd15883f7c7e53377a022dbc385da
Kristoffer Walker <kris@kixx.name>
2014-04-22 23:11:17 -0400

    First cadre of failing tests.
    	new file:   lib/blues_brothers.js
    	new file:   lib/distances.js
    	new file:   lib/elevations.js
    	new file:   lib/index.js
    	new file:   lib/lost_cities.js
    	new file:   lib/nicknames.js
    	new file:   lib/our_town.js
    	new file:   lib/places.js
    	modified:   package.json
    	modified:   test/test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,56 @@
+// Editing anything in this file is cheating! :)
+// Getting these tests to pass should go from easier to harder as you move
+// down the file.
+
 require('should');
 
 describe("CommonJS modules in Node.js", function () {
-  it("should not be smoking", function (done) {
-    true.should.eql(true);
-    return done();
+
+  describe("exported properties", function () {
+
+    it("knows some city nicknames", function () {
+      var places = require('../lib/nicknames');
+      places.chicago.nickname.should.eql('windy city');
+      places.boston.nickname.should.eql('beantown');
+      places.denver.nickname.should.eql('mile high city');
+    });
+  });
+
+  describe("module id", function () {
+
+    it("should *not* raise an exception", function () {
+      require('../lib/blues_brothers').areGoingTo.should.eql('Chicago');
+    });
+  });
+
+  describe("package module id", function () {
+
+    it("should *not* raise an exception", function () {
+      require('../lib/elevations').denver.should.eql(5280);
+    });
+  });
+
+  describe("synchronous loading and evaluation", function (done) {
+
+    it("can't set exports asynchronously", function () {
+      require('../lib/distances').fromNewYork2Chicago.should.eql(807);
+    });
+  });
+
+  describe("module closures", function () {
+
+    it("should keep secrets", function () {
+      require('../lib/lost_cities');
+      (typeof TROY).should.eql('undefined');
+      (typeof MACHU_PICCHU).should.eql('undefined');
+      (typeof POMPEII).should.eql('undefined');
+    });
+  });
+
+  describe("circular dependencies", function () {
+
+    it("should compute the name of our town", function () {
+      require('../lib/our_town').say().should.eql('Saratoga Springs NY');
+    });
   });
 });
